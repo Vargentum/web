94678501849abe36e27df090beb8270adbdbdf6a
Vladislav Serebriakov <vladislav.serebriakov@x2sy.com>
2022-03-25 16:16:44 +0200

    final

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
 import logo from '../img/logo.svg';
+import { useQuery } from '@apollo/client';
+import { Link, withRouter } from 'react-router-dom';
+
+import ButtonAsLink from './ButtonAsLink';
+import { IS_LOGGED_IN } from '../gql/query';
 
 const HeaderBar = styled.header`
   width: 100%;
@@ -20,13 +25,44 @@ const LogoText = styled.h1`
   display: inline;
 `;
 
-const Header = () => {
+const UserState = styled.div`
+  margin-left: auto;
+`;
+
+const Header = props => {
+  // query hook for user logged in state
+  const { data, client } = useQuery(IS_LOGGED_IN);
+
   return (
     <HeaderBar>
       <img src={logo} alt="Notedly Logo" height="40" />
       <LogoText>Notedly</LogoText>
+      {/* If logged in display a log out link, else display sign in options */}
+      <UserState>
+        {data.isLoggedIn ? (
+          <ButtonAsLink
+            onClick={() => {
+              // remove the token
+              localStorage.removeItem('token');
+              // clear the application's cache
+              client.resetStore();
+              // update local state
+              client.writeData({ data: { isLoggedIn: false } });
+              // redirect the user to the homepage
+              props.history.push('/');
+            }}
+          >
+            Logout
+          </ButtonAsLink>
+        ) : (
+          <p>
+            <Link to={'/signin'}>Sign In</Link> or{' '}
+            <Link to={'/signup'}>Sign Up</Link>
+          </p>
+        )}
+      </UserState>
     </HeaderBar>
   );
 };
 
-export default Header;
+export default withRouter(Header);
